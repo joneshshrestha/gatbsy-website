c7187aa55e17051f51119b7dbe8c7a9b0736a269
joneshshrestha <contact@joneshshrestha.com>
2020-08-19 14:52:09 +0545

    contact and about page  added

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <h2>Grounded and solution-oriented Computer Scientist with a wide variety of experience. Adept at motivating self and others. Passionate about web development and technology.</h2>
+        </div>
+    )
+}
+
+export default AboutPage
---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ContactPage = () => {
+    return (
+        <div>
+            <h1>Contact</h1>
+            <h2>email: contact@joneshshrestha.com</h2>
+            <h2>mobile: +977 9863475245</h2>
+            <h2>Kathmandu, Nepal</h2>
+        </div>
+    )
+}
+
+export default ContactPage
