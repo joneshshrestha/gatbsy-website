5dd6801f29bc9a2d48c9f552e6cb161489bf5999
joneshshrestha <contact@joneshshrestha.com>
2020-08-19 14:45:20 +0545

    blog page added

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BlogPage = () => {
+    return (
+        <div>
+            <h1>Blog.</h1>
+            <h2>Posts will show up later here.</h2>
+        </div>
+    )
+}
+
+export default BlogPage
