16edfb0b119316a8e9cb753348fbedd7de93ee4c
joneshshrestha <contact@joneshshrestha.com>
2020-08-20 12:44:48 +0545

    header added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import Header from '../components/header'
 import Footer from '../components/footer'
 
 const indexPage = () => {
   return (
     <div>
+      < Header />
       <h1>Hello.</h1>
       <h2>I am Jonesh, a web developer living in Nepal.</h2>
       <p>Need Developer? <Link to='/contact'>Contact</Link></p>
