e5e090dda7fb57748a485cf50e31374f8ed454d2
joneshshrestha <contact@joneshshrestha.com>
2020-08-22 13:18:12 +0545

    layout.module.scss file updated

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,10 +2,11 @@ import React from 'react'
 import Header from '../components/header'
 import Footer from '../components/footer'
 import '../styles/index.scss'
+import layoutStyles from './layout.module.scss'
 
 const Layout = (props) => {
     return (
-        <div>
+        <div className= {layoutStyles.container}>
             <Header />
             {props.children}
             <Footer />
