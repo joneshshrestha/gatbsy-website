325870df7c4a9e40f5c6ce36550b083a206c9d72
joneshshrestha <contact@joneshshrestha.com>
2020-08-24 23:14:46 +0545

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,11 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: 'Gatsby Website',
+    author: 'Jonesh Shrestha',
+    
+  },
   plugins: [
     'gatsby-plugin-sass'
   ],
